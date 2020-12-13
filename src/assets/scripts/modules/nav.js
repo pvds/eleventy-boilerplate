d7e85127af1195457da0213c46174f604a30e11d
Peter van der Steen <peter.van.der.steen@priva.nl>
2020-12-13 15:40:21 +0100

    feat: update navigation

--- src/assets/scripts/modules/nav.js
+++ src/assets/scripts/modules/nav.js
@@ -1,8 +1,12 @@
 import { createFocusTrap } from 'focus-trap';
+import { debounce, focusable } from './helpers';
 
 const SELECTORS = {
   nav: '.js-nav',
-  toggleBtn: '.js-nav-toggle',
+  navLinks: '.js-nav-link',
+  ctaLink: '.js-nav-cta',
+  openBtn: '.js-nav-open',
+  closeBtn: '.js-nav-close',
 };
 
 const CLASSES = {
@@ -22,26 +26,65 @@ class Navigation {
     this.isOpen = false;
 
     this.nav = document.querySelector(SELECTORS.nav);
-    this.toggleBtn = this.nav.querySelector(SELECTORS.toggleBtn);
-    this.focusTrap = createFocusTrap(this.nav);
+    this.openBtn = this.nav.querySelector(SELECTORS.openBtn);
+    this.closeBtn = this.nav.querySelector(SELECTORS.closeBtn);
+    this.navLinks = this.nav.querySelectorAll(SELECTORS.navLinks);
+    this.ctaLink = this.nav.querySelector(SELECTORS.ctaLink);
+    this.focusTrap = createFocusTrap(this.nav, { allowOutsideClick: true });
 
-    this.toggleBtn.addEventListener('click', () => this.toggleMenu());
+    this.openBtn.addEventListener('click', () => this.toggleMenu(true));
+    this.closeBtn.addEventListener('click', () => this.toggleMenu(false));
+    this.close = this.toggleMenu(false);
+
+    this.clickOutsideMenu = (e) => {
+      if (!this.nav.contains(e.target)) return this.toggleMenu(false);
+    };
+
+    this.keyNav = (e) => {
+      switch (e.code) {
+        case 'Escape':
+          this.toggleMenu(false);
+          break;
+        case 'KeyM':
+          this.toggleMenu();
+          break;
+      }
+    };
+
+    document.addEventListener('keydown', debounce(this.keyNav, 200, true));
   }
 
   /**
    * Toggle navigation menu
    * @param {boolean} force
    */
-  toggleMenu(force) {
+  toggleMenu(force = undefined) {
     this.isOpen = typeof force === 'boolean' ? force : !this.isOpen;
 
     this.nav.classList.toggle(CLASSES.open, this.isOpen);
-    this.toggleBtn.setAttribute('aria-expanded', String(this.isOpen));
+    this.openBtn.setAttribute('aria-expanded', String(this.isOpen));
+    this.closeBtn.setAttribute('aria-expanded', String(this.isOpen));
 
     if (this.isOpen) {
       this.focusTrap.activate();
+      window.addEventListener('click', this.clickOutsideMenu, true);
+
+      focusable(this.ctaLink, true);
+      focusable(this.closeBtn, true);
+
+      [...this.navLinks].map((link) => {
+        focusable(link, true);
+      });
     } else {
       this.focusTrap.deactivate();
+      window.removeEventListener('click', this.clickOutsideMenu, true);
+
+      focusable(this.ctaLink, false);
+      focusable(this.closeBtn, false);
+
+      [...this.navLinks].map((link) => {
+        focusable(link, false);
+      });
     }
   }
 }
