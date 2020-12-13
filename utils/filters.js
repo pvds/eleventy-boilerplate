fc03b566396bc591f229f43444d3024c53022025
Peter van der Steen <peter.van.der.steen@priva.nl>
2020-12-13 15:02:24 +0100

    style: cleanup filters

--- utils/filters.js
+++ utils/filters.js
@@ -3,32 +3,30 @@ const { DateTime } = require('luxon');
 module.exports = {
   /**
    * Format date to given format
-   * @param {string} date
+   * @param {Date} date
    * @param {string} format
    * @return {string}
    */
-  dateToFormat: function (date, format) {
-    return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(String(format));
-  },
+  dateToFormat: (date, format) =>
+    DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(String(format)),
 
   /**
    * Format date to ISO format
-   * @param {string} date
+   * @param {Date} date
    * @return {string}
    */
-  dateToISO: function (date) {
-    return DateTime.fromJSDate(date, { zone: 'utc' }).toISO({
+  dateToISO: (date) =>
+    DateTime.fromJSDate(date, { zone: 'utc' }).toISO({
       includeOffset: false,
       suppressMilliseconds: true,
-    });
-  },
+    }),
 
   /**
    * Obfuscate string
    * @param {string} str
    * @return {string}
    */
-  obfuscate: function (str) {
+  obfuscate: (str) => {
     const chars = [];
     for (let i = str.length - 1; i >= 0; i--) {
       chars.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
